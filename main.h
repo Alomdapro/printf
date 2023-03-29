73cc053dd1afefad9eed19e7064d820aa3b563d2
Ahmed Mansour <ahmedkamel120012@gmail.com>
2023-03-29 21:22:20 +0200

    update main.h

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <unistd.h>
 /**
 * struct format - match the conversion specifiers for printf
-* @id: type char pointer of the specifier 
+* @id: type char pointer of the specifier
 * @f: type pointer to function for the conversion specifier
 *
 */
